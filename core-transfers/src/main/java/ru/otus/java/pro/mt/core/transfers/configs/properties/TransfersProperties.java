f1139392823fca7d19078b6f57deec362d0a084c
Dmitry Petrov <166365976+pds05@users.noreply.github.com>
2025-03-07 05:07:14 +0400

    refactor: exclude unused dependencies

--- core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/configs/properties/TransfersProperties.java
+++ core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/configs/properties/TransfersProperties.java
@@ -1,9 +1,7 @@
 package ru.otus.java.pro.mt.core.transfers.configs.properties;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 import java.math.BigDecimal;
 import java.util.Set;
